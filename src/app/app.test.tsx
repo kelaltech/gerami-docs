c4273b9863932cca7036b3ec21fc3dabedbf06db
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2019-02-05 16:32:17 +0300

    Improved app.test.tsx

--- src/app/app.test.tsx
+++ src/app/app.test.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { App } from './app'
+
+import fontawesomeLibrary from '../shared/configs/fontawesome-library'
+import App from './app'
+
+beforeAll(() => {
+  fontawesomeLibrary() // fontawesome icons
+})
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
